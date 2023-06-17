ff50c87951eb21333f62da2461c34e1d7163748e
David Zhang <v8os@icloud.com>
2023-06-17 22:52:21 +0800

    Toggle text content for Mute/UnMute button
    Fix #30

--- static/script.js
+++ static/script.js
@@ -7,3 +7,20 @@ form.addEventListener("submit", async (event) => {
   document.querySelector(".container").style.display = "none";
   document.querySelector("#video-container").style.display = "flex";
 });
+
+async function onBtnClick(event) {
+  var btn = event.target;
+  var status = btn.getAttribute("data-status");
+  if (status === "active") {
+    btn.textContent = btn.textContent.replace("Mute", "UnMute");
+    btn.setAttribute("data-status", "muted");
+  } else if (status === "muted") {
+    btn.textContent = btn.textContent.replace("UnMute", "Mute");
+    btn.setAttribute("data-status", "active");
+  }
+}
+
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", onBtnClick);
+}
