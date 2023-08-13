0f21af5b6c7eb50a0bc9337c8d00a38dcf5a987a
William <williamlsh@protonmail.com>
2023-08-13 21:03:18 +0000

    Add media device availability check

--- peer/src/media.rs
+++ peer/src/media.rs
@@ -12,8 +12,8 @@ use js_sys::Array;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{
-    DisplayMediaStreamConstraints, HtmlMediaElement, MediaStream, MediaStreamConstraints,
-    MediaStreamTrack, RtcPeerConnection,
+    DisplayMediaStreamConstraints, HtmlMediaElement, MediaDeviceInfo, MediaDeviceKind,
+    MediaDevices, MediaStream, MediaStreamConstraints, MediaStreamTrack, RtcPeerConnection,
 };
 
 pub(crate) async fn init(pc: &RtcPeerConnection) -> Result<(), JsValue> {
@@ -29,9 +29,19 @@ async fn handle_local_stream(
 ) -> Result<(), JsValue> {
     // We receive the first message since there will only be one user sharing option.
     let local_stream = match rx.next().await.unwrap() {
-        UserSharingOption::Media => get_user_media(true).await?,
+        UserSharingOption::Media => {
+            get_user_media(
+                check_media_device(&MediaDeviceKind::Videoinput).await?,
+                check_media_device(&MediaDeviceKind::Audioinput).await?,
+            )
+            .await?
+        }
         UserSharingOption::Screen => {
-            let media_stream = get_user_media(false).await?;
+            let media_stream = get_user_media(
+                false,
+                check_media_device(&MediaDeviceKind::Audioinput).await?,
+            )
+            .await?;
             let display_stream = get_display_media().await?;
             let tracks = media_stream
                 .get_tracks()
@@ -58,17 +68,14 @@ async fn handle_local_stream(
     Ok(())
 }
 
-async fn get_user_media(enable_video: bool) -> Result<MediaStream, JsValue> {
+async fn get_user_media(enable_video: bool, enable_audio: bool) -> Result<MediaStream, JsValue> {
     Ok(MediaStream::from(
         JsFuture::from(
-            window()
-                .navigator()
-                .media_devices()?
-                .get_user_media_with_constraints(
-                    MediaStreamConstraints::new()
-                        .video(&JsValue::from_bool(enable_video))
-                        .audio(&JsValue::from_bool(true)),
-                )?,
+            media_devices()?.get_user_media_with_constraints(
+                MediaStreamConstraints::new()
+                    .video(&JsValue::from_bool(enable_video))
+                    .audio(&JsValue::from_bool(enable_audio)),
+            )?,
         )
         .await?,
     ))
@@ -77,9 +84,7 @@ async fn get_user_media(enable_video: bool) -> Result<MediaStream, JsValue> {
 async fn get_display_media() -> Result<MediaStream, JsValue> {
     Ok(MediaStream::from(
         JsFuture::from(
-            window()
-                .navigator()
-                .media_devices()?
+            media_devices()?
                 .get_display_media_with_constraints(&DisplayMediaStreamConstraints::default())?,
         )
         .await?,
@@ -95,4 +100,22 @@ fn display_local_video(track: &MediaStreamTrack) {
     get_element_by_id::<HtmlMediaElement>("local-video")
         .expect("#local-video should be an `HtmlVideoElement`")
         .set_src_object(video_stream.dyn_ref());
+}
+
+async fn check_media_device(kind: &MediaDeviceKind) -> Result<bool, JsValue> {
+    Ok(
+        Array::from(&JsFuture::from(media_devices()?.enumerate_devices()?).await?)
+            .iter()
+            .any(|media_device_info: JsValue| {
+                media_device_info
+                    .dyn_into::<MediaDeviceInfo>()
+                    .unwrap()
+                    .kind()
+                    .eq(kind)
+            }),
+    )
+}
+
+fn media_devices() -> Result<MediaDevices, JsValue> {
+    window().navigator().media_devices()
 }
